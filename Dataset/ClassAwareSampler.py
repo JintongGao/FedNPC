c22308c2190de0a772eb3f3ab40429aff5cd3dab
gaojt23 <128790241+JintongGao@users.noreply.github.com>
2026-03-16 20:13:10 +0800

    Update ClassAwareSampler.py

--- Dataset/ClassAwareSampler.py
+++ Dataset/ClassAwareSampler.py
@@ -1,17 +1,6 @@
 """Copyright (c) Facebook, Inc. and its affiliates.
 All rights reserved.
 
-This source code is licensed under the license found in the
-LICENSE file in the root directory of this source tree.
-
-Portions of the source code are from the OLTR project which
-notice below and in LICENSE in the root directory of
-this source tree.
-
-Copyright (c) 2019, Zhongqi Miao
-All rights reserved.
-"""
-
 import random
 import numpy as np
 from torch.utils.data.sampler import Sampler
